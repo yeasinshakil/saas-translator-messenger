18e23193d34316e90bb0d659df1a99550d6a1ead
yeasinshakil <shakilrock1998@gmail.com>
2023-11-11 15:10:22 +0600

    home page design

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <ClientProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body className="flex flex-col min-h-screen">
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
